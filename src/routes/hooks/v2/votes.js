bafe2fc04dc1dd568b68629b9343120e600fc38e
TheRealToxicDev <toxic.dev09@gmail.com>
2023-03-30 19:48:18 -0600

    feat(update): Webhooks v2 support

---
+++ src/routes/hooks/v2/votes.js
@@ -0,0 +1,49 @@
+const { MessageEmbed } = require("discord.js");
+const { Reedhook } = require("@infinitybots/client");
+const config = require("../../config");
+const moment = require("moment");
+
+const webhook = new Reedhook(`${config.hook_secret}`);
+
+module.exports = async (fastify, opts) => {
+  fastify.post(
+    "/votes",
+    webhook.hookListener(async (voteData, req, res) => {
+      let client = req.client;
+
+      let embed = new MessageEmbed()
+        .setTitle(`Vote Logs`)
+        .setColor("RANDOM")
+        .setDescription(`Woah someone has voted for me on Infinity Bot List`)
+        .addFields(
+          {
+            name: "Total Votes",
+            value: `${voteData.data.votes}`,
+            inline: true,
+          },
+          {
+            name: "User",
+            value: `${voteData.creator.username}#${voteData.creator.discriminator}`,
+            inline: true,
+          },
+          {
+            name: "Time",
+            value: `${moment(voteData.created_at)}`,
+            inline: true,
+          }
+        )
+        .setTimestamp()
+        .setFooter({
+          text: "Infinity Vote Logger",
+          iconURL: `${voteData.creator.avatar}`,
+        });
+
+      await res.status(204).send("Success");
+
+      return client.guilds.cache
+        .get(config.guildID)
+        .channels.cache.get(config.voteLogs)
+        .send({ embeds: [embed] });
+    })
+  );
+};
